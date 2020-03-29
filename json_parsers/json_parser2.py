62c7c3aa142ad98a9e413ec2825e63c392bae5d5
rajakumaro <rajakumaroleti@gmail.com>
2020-03-29 23:01:00 +0530

    retieve data from json files third commit

--- json_parsers/json_parser2.py
+++ json_parsers/json_parser2.py
@@ -1,30 +1,42 @@
 
 import json
 
-class JSON: 
+class JsonReader:
     
-    def json_read(self,path):
-    
-        f=open(path,'r')
-
+    def read_file(self,filepath):
+        f=open(filepath,'r')
         data=f.read()
-
         f.close()
+        return data
 
-        x=json.loads(data)
+    def load_json_data(self,data):
+        json_text=json.loads(data)
+        return json_text
     
-        return x
+    def get_ids(self,path,json_text):
+        return  [ "id1",json_text["data"][0]["_id"] , "id2",json_text["data"][1]["_id"] ]
+
+    def mongo_db(self,path,json_text):
+        return  json_text["mongodb"]
 
 if __name__ == "__main__":
+
+    Json_obj=JsonReader()
     
-    j=JSON()
+    path1 = "D:\\Work\\automation-frameworks\\json_parsers\\input.json"
+    out_put1 = Json_obj.read_file(path1)
+    load_data1=Json_obj.load_json_data(out_put1)
+    ids=Json_obj.get_ids(path1,load_data1)
+    print(ids)
     
-    path1 = "D:\\python\\input.json"
-    out_put1 = j.json_read(path1)
-    print ("id1:",out_put1["data"][0]["_id"])
-    print ("id2:",out_put1["data"][1]["_id"])
+
+    path2 = "D:\\Work\\automation-frameworks\\json_parsers\\input2.json"
+    out_put2 = Json_obj.read_file(path2)
+    load_data2=Json_obj.load_json_data(out_put2)
+    mongodb_val=Json_obj.mongo_db(path2,load_data2)
+    print(mongodb_val)
     
-    path2 = "D:\\python\\input2.json"
-    out_put2 = j.json_read(path2)
-    print (out_put2["mongodb"])
-    print(type(out_put2))
+    
+    
+
+
