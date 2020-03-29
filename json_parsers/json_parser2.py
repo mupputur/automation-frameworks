fca2ce5f6a32b7f5436f5b0e96da272fbacb7b68
rajakumaro <rajakumaroleti@gmail.com>
2020-03-29 19:33:07 +0530

    retrieved the data from json file

--- json_parsers/json_parser2.py
+++ json_parsers/json_parser2.py
@@ -1,36 +1,30 @@
-import json
-
-path="D:\\python\\input.json"
-
-f=open(path,'r')
-
-data=f.read()
-
-f.close()
-
-x=json.loads(data)
-
-print("id1:",x["data"][0]["_id"])
-print("id2:",x["data"][1]["_id"])
-
-
-print("*"*60)
 
 import json
 
-path="D:\\python\\input2.json"
+class JSON: 
+    
+    def json_read(self,path):
+    
+        f=open(path,'r')
 
+        data=f.read()
 
-f=open(path,'r')
+        f.close()
 
-data=f.read()
+        x=json.loads(data)
+    
+        return x
 
-f.close()
-
-x=json.loads(data)
-
-print(x["mongodb"])
-
-print(type(x))
-
-
+if __name__ == "__main__":
+    
+    j=JSON()
+    
+    path1 = "D:\\python\\input.json"
+    out_put1 = j.json_read(path1)
+    print ("id1:",out_put1["data"][0]["_id"])
+    print ("id2:",out_put1["data"][1]["_id"])
+    
+    path2 = "D:\\python\\input2.json"
+    out_put2 = j.json_read(path2)
+    print (out_put2["mongodb"])
+    print(type(out_put2))
