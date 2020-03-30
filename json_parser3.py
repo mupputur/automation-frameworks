1aade77d818c97e3a2b69ec6929acb140eb1d024
unknown <you@example.com>
2020-03-30 12:44:39 +0530

    frist json_files commited

--- json_parser3.py
+++ json_parser3.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     path="C:\\Users\\Admin\\Documents\\json\\json1.txt"
     out_put=j.json_read(path)
     print(out_put.keys())
+    
     print(out_put.values())
 
     print("="*50)
@@ -22,6 +23,7 @@ if __name__ == "__main__":
     path="C:\\Users\\Admin\\Documents\\json\\json2.txt"
     out_put2=j.json_read(path)
     print(out_put2.keys())
+    
     print(out_put2.values())
 
     
