5883aafe3d6969056ba0bcabe512d68e263e6ea0
RavithejaDuggirala7777 <104202261+RavithejaDuggirala7777@users.noreply.github.com>
2022-07-10 13:00:33 +0530

    adding the new file utils and modified the previous code

---
+++ guru99/Utils.py
@@ -0,0 +1,7 @@
+
+url = "http://www.demo.guru99.com/V4/"
+user_name = "uid"
+pass_word = "password"
+bot_ton = "btnLogin"
+path = executable_path="C:\\Users\\jagad\\Dropbox\\PC\Desktop\\automation_framework\\dependencies\\drivers\\chrome\\chromedriver.exe"
+xl_path = "C:\\Users\\jagad\\Desktop\\final_project\\automation-frameworks\\guru99\\test_data\\Test_data.xlsx"
